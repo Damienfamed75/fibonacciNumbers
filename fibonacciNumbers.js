f33f3c8804c1cc523f711ab387be08486e1c0e9f
Damien Stamates <thedoubledimensions@gmail.com>
2017-08-31 19:34:19 -0500

    Refactored code
    refactored into one line

--- fibonacciNumbers.js
+++ fibonacciNumbers.js
@@ -1,21 +1,11 @@
 ///////////////////////////////////////////////////////////
 ////             Made by: Damien Stamates              ////
-////                 Version: 0.1.01                   ////
+////                 Version: 0.3.01                   ////
 ////   Summary: A function to list a specific number   ////
 ////      of fibonacci numbers within the sequence     ////
 ///////////////////////////////////////////////////////////
 
 
-fib=n=>{
-    var a = 1, b = 0, temp; // creates all needed variables
-    while(n >= 0) { // keeps listing the sequence until given number = 0
-        temp = a; // temp value = a's current value
-        a = a + b; // changing a to equal a + b
-        b = temp; // b = a's previous value
-        n--; // subtracts given value by one
-        console.log(a); // logs the value of a
-    }
-    return b; // returns value of b
-}
+fib=n=>{var a=1,b=0,t;while(n>=0){t=a;a=a+b;b=t;n--;console.log(a)}return b}
 
-fib(25); // Calls function
+fib(100);
